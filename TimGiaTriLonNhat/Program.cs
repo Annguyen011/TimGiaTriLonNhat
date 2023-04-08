85d5d65ab10ed4e194702964affe2d5d4a3856d4
Anngu <Anngu@MSI>
2023-04-08 17:24:45 +0700

    Add project files.

---
+++ TimGiaTriLonNhat/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimGiaTriLonNhat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arr = new int[,]
+{
+    {1, 2, 3},
+    {4, 5, 6},
+    {7, 8, 9}
+};
+
+            int max = arr[0, 0];
+
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    if (arr[i, j] > max)
+                    {
+                        max = arr[i, j];
+                    }
+                }
+            }
+
+            Console.WriteLine("Max value in the array: " + max);
+
+        }
+    }
+}
